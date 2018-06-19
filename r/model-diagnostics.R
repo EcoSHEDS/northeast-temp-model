b93629dcdfb8d5a9602360bdac776b5222d3e5f3
Jeff Walker <walker.jeff.d@gmail.com>
2018-06-19 17:14:51 -0400

    revise model-diagnostics, add summaries

--- r/model-diagnostics.R
+++ r/model-diagnostics.R
@@ -1,5 +1,6 @@
 # generate model diagnostics
 # <- {wd}/model-output.rds
+# -> {wd}/model-diagnostics.rds
 
 rm(list=ls())
 
@@ -26,70 +27,72 @@ config <- load_config()
 
 out <- readRDS(file.path(config$wd, "model-output.rds"))
 
-B.0 <- out$results$mean$B.0
-X.0 <- out$data$X.0
-Y.0 <- (X.0 %*% as.matrix(B.0))[, 1]
+# calibration -------------------------------------------------------------
 
-B.site <- out$results$mean$B.site[out$data$site, ]
-X.site <- out$data$X.site
-Y.site <- rowSums(X.site * B.site)
+B.fixed <- out$results$mean$B.0
+X.fixed <- out$data$X.0
+Y.fixed <- (X.fixed %*% as.matrix(B.fixed))[, 1]
+
+B.catchment <- out$results$mean$B.site[out$data$site, ]
+X.catchment <- out$data$X.site
+Y.catchment <- rowSums(X.catchment * B.catchment)
 
 B.year <- as.matrix(out$results$mean$B.year[out$data$year, ])
 X.year <- out$data$X.year
 Y.year <- rowSums(X.year * B.year)
 
-B.huc <- as.matrix(out$results$mean$B.huc[out$data$huc, ])
-X.huc <- out$data$X.site
-Y.huc <- rowSums(X.huc * B.huc)
+B.huc8 <- as.matrix(out$results$mean$B.huc[out$data$huc, ])
+X.huc8 <- out$data$X.site
+Y.huc8 <- rowSums(X.huc8 * B.huc8)
 
-Y <- Y.0 + Y.site + Y.year + Y.huc
+Y <- Y.fixed + Y.catchment + Y.huc8 + Y.year
 
-df_pred <- data_frame(
+df_calib <- data_frame(
   date = out$df$date,
-  site = out$df$site,
-  year = out$df$year,
   huc8 = out$df$huc8,
+  catchment = out$df$site,
+  year = out$df$year,
   deploy_id = out$df$deploy_id,
-  temp = out$df$temp,
-  Y = Y
+  obs = out$df$temp,
+  trend = Y
 ) %>%
   group_by(deploy_id) %>%
   mutate(
     deploy_row = row_number(),
-    resid_Y = temp - Y,
-    resid_lag = coalesce(lag(resid_Y), 0),
-    pred = if_else(deploy_row == 1, Y, Y + out$results$mean$B.ar1 * resid_lag),
-    resid = temp - pred
+    resid_trend = obs - trend,
+    resid_lag = coalesce(lag(resid_trend), 0),
+    pred = if_else(deploy_row == 1, trend, trend + out$results$mean$B.ar1 * resid_lag),
+    resid = obs - pred
   ) %>%
   ungroup()
 
-df_pred_deploy <- df_pred %>%
-  group_by(site, year, deploy_id) %>%
+df_calib_deploy <- df_calib %>%
+  group_by(catchment, year, deploy_id) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_Y = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_Y^2))
+    rmse_trend = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_trend^2))
     })
   )
 
-df_pred_site <- df_pred %>%
-  group_by(site) %>%
+df_calib_catchment <- df_calib %>%
+  group_by(catchment) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_Y = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_Y^2))
+    rmse_trend = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_trend^2))
     })
   )
 
-df_pred_huc <- df_pred %>%
+df_calib_huc8 <- df_calib %>%
   group_by(huc8) %>%
   nest() %>%
   mutate(
@@ -97,108 +100,209 @@ df_pred_huc <- df_pred %>%
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_Y = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_Y^2))
+    rmse_trend = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_trend^2))
     })
   )
 
+df_calib_summary <- df_calib %>%
+  summarise(
+    n_obs = n(),
+    n_catchment = length(unique(catchment)),
+    n_huc8 = length(unique(huc8)),
+    n_deploy = length(unique(deploy_id)),
+    n_year = length(unique(year)),
+
+    rmse = sqrt(mean(resid ^ 2)),
+    mean_resid = mean(resid),
+    median_resid = median(resid),
+    mean_abs_resid = mean(abs(resid)),
+    median_abs_resid = median(abs(resid)),
+    min_resid = min(resid),
+    max_resid = max(resid),
+    q01_resid = quantile(resid, probs = 0.01),
+    q10_resid = quantile(resid, probs = 0.1),
+    q25_resid = quantile(resid, probs = 0.25),
+    q75_resid = quantile(resid, probs = 0.75),
+    q90_resid = quantile(resid, probs = 0.9),
+    q99_resid = quantile(resid, probs = 0.99),
+
+    rmse_trend = sqrt(mean(resid_trend ^ 2)),
+    mean_resid_trend = mean(resid_trend),
+    median_resid_trend = median(resid_trend),
+    mean_abs_resid_trend = mean(abs(resid_trend)),
+    median_abs_resid_trend = median(abs(resid_trend)),
+    min_resid_trend = min(resid_trend),
+    max_resid_trend = max(resid_trend),
+    q01_resid_trend = quantile(resid_trend, probs = 0.01),
+    q10_resid_trend = quantile(resid_trend, probs = 0.1),
+    q25_resid_trend = quantile(resid_trend, probs = 0.25),
+    q75_resid_trend = quantile(resid_trend, probs = 0.75),
+    q90_resid_trend = quantile(resid_trend, probs = 0.9),
+    q99_resid_trend = quantile(resid_trend, probs = 0.99)
+  ) %>%
+  as.list()
+
+
+# validation --------------------------------------------------------------
 
 df_valid <- readRDS(file.path(config$wd, "model-input.rds"))$test %>%
   select(-site_id, -huc8_id, -year_id) %>%
   left_join(out$ids$site, by = "site") %>%
   left_join(out$ids$year, by = "year") %>%
-  left_join(out$ids$huc8, by = "huc8")
+  left_join(out$ids$huc8, by = "huc8") %>%
+  rename(
+    obs = temp,
+    catchment = site,
+    catchment_id = site_id,
+    new_catchment = new_site
+  )
 summary(df_valid)
 
-X.0.pred <- df_valid %>%
+X.fixed_valid <- df_valid %>%
   mutate(intercept = 1) %>%
   select(one_of(out$covs$fixed.ef))
-B.0.pred <- out$results$mean$B.0
-Y.0.pred <- (as.matrix(X.0.pred) %*% as.matrix(B.0.pred))[, 1]
+B.fixed_valid <- out$results$mean$B.0
+Y.fixed_valid <- (as.matrix(X.fixed_valid) %*% as.matrix(B.fixed_valid))[, 1]
 
-X.site.pred <- df_valid %>%
+X.catchment_valid <- df_valid %>%
   mutate(intercept.site = 1) %>%
   select(one_of(out$covs$site.ef))
-B.site.mean <- colMeans(out$results$mean$B.site)
-B.site.pred <- out$results$mean$B.site[df_valid$site_id, ]
-for (i in seq_along(B.site.mean)) {
-  B.site.pred[is.na(B.site.pred[, i]), i] <- B.site.mean[i]
+B.catchment.mean <- colMeans(out$results$mean$B.site)
+B.catchment_valid <- out$results$mean$B.site[df_valid$catchment_id, ]
+for (i in seq_along(B.catchment.mean)) {
+  B.catchment_valid[is.na(B.catchment_valid[, i]), i] <- B.catchment.mean[i]
 }
-Y.site.pred <- rowSums(X.site.pred * B.site.pred)
+Y.catchment_valid <- rowSums(X.catchment_valid * B.catchment_valid)
 
-X.huc.pred <- X.site.pred
-B.huc.mean <- colMeans(out$results$mean$B.huc)
-B.huc.pred <- out$results$mean$B.huc[df_valid$huc8_id, ]
-for (i in seq_along(B.huc.mean)) {
-  B.huc.pred[is.na(B.huc.pred[, i]), i] <- B.huc.mean[i]
+X.huc8_valid <- X.catchment_valid
+B.huc8.mean <- colMeans(out$results$mean$B.huc)
+B.huc8_valid <- out$results$mean$B.huc[df_valid$huc8_id, ]
+for (i in seq_along(B.huc8.mean)) {
+  B.huc8_valid[is.na(B.huc8_valid[, i]), i] <- B.huc8.mean[i]
 }
-Y.huc.pred <- rowSums(X.huc.pred * B.huc.pred)
+Y.huc8_valid <- rowSums(X.huc8_valid * B.huc8_valid)
 
-X.year.pred <- df_valid %>%
+X.year_valid <- df_valid %>%
   mutate(intercept.year = 1) %>%
   select(one_of(out$covs$year.ef))
 B.year.mean <- colMeans(out$results$mean$B.year)
-B.year.pred <- as.matrix(out$results$mean$B.year[df_valid$year_id, ])
+B.year_valid <- as.matrix(out$results$mean$B.year[df_valid$year_id, ])
 for (i in seq_along(B.year.mean)) {
-  B.year.pred[is.na(B.year.pred[, i]), i] <- B.year.mean[i]
+  B.year_valid[is.na(B.year_valid[, i]), i] <- B.year.mean[i]
 }
-Y.year.pred <- rowSums(X.year.pred * B.year.pred)
+Y.year_valid <- rowSums(X.year_valid * B.year_valid)
 
-Y.pred <- Y.0.pred + Y.site.pred + Y.year.pred + Y.huc.pred
+Y_valid <- Y.fixed_valid + Y.huc8_valid + Y.catchment_valid + Y.year_valid
 
 df_valid <- df_valid %>%
   mutate(
-    Y = Y.pred
+    trend = Y_valid
   ) %>%
   group_by(deploy_id) %>%
   mutate(
     deploy_row = row_number(),
-    resid_Y = temp - Y,
-    resid_lag = coalesce(lag(resid_Y), 0),
-    pred = if_else(deploy_row == 1, Y, Y + out$results$mean$B.ar1 * resid_lag),
-    resid = temp - pred
+    resid_trend = obs - trend,
+    resid_lag = coalesce(lag(resid_trend), 0),
+    pred = if_else(deploy_row == 1, trend, trend + out$results$mean$B.ar1 * resid_lag),
+    resid = obs - pred
   ) %>%
   ungroup()
 
-
 df_valid_deploy <- df_valid %>%
-  group_by(site, year, deploy_id) %>%
+  group_by(catchment, year, deploy_id) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_Y = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_Y^2))
+    rmse_trend = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_trend^2))
     })
   )
 
-df_valid_site <- df_valid %>%
-  group_by(site) %>%
+df_valid_catchment <- df_valid %>%
+  group_by(catchment) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_Y = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_Y^2))
+    rmse_trend = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_trend^2))
     })
   )
+
+
+df_valid_huc8 <- df_valid %>%
+  group_by(huc8) %>%
+  nest() %>%
+  mutate(
+    n = map_int(data, nrow),
+    rmse = map_dbl(data, function (x) {
+      sqrt(mean(x$resid^2))
+    }),
+    rmse_trend = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_trend^2))
+    })
+  )
+
+df_valid_summary <- df_valid %>%
+  summarise(
+    n_obs = n(),
+    n_catchment = length(unique(catchment)),
+    n_huc8 = length(unique(huc8)),
+    n_deploy = length(unique(deploy_id)),
+    n_year = length(unique(year)),
+
+    rmse = sqrt(mean(resid ^ 2)),
+    mean_resid = mean(resid),
+    median_resid = median(resid),
+    mean_abs_resid = mean(abs(resid)),
+    median_abs_resid = median(abs(resid)),
+    min_resid = min(resid),
+    max_resid = max(resid),
+    q01_resid = quantile(resid, probs = 0.01),
+    q10_resid = quantile(resid, probs = 0.1),
+    q25_resid = quantile(resid, probs = 0.25),
+    q75_resid = quantile(resid, probs = 0.75),
+    q90_resid = quantile(resid, probs = 0.9),
+    q99_resid = quantile(resid, probs = 0.99),
+
+    rmse_trend = sqrt(mean(resid_trend ^ 2)),
+    mean_resid_trend = mean(resid_trend),
+    median_resid_trend = median(resid_trend),
+    mean_abs_resid_trend = mean(abs(resid_trend)),
+    median_abs_resid_trend = median(abs(resid_trend)),
+    min_resid_trend = min(resid_trend),
+    max_resid_trend = max(resid_trend),
+    q01_resid_trend = quantile(resid_trend, probs = 0.01),
+    q10_resid_trend = quantile(resid_trend, probs = 0.1),
+    q25_resid_trend = quantile(resid_trend, probs = 0.25),
+    q75_resid_trend = quantile(resid_trend, probs = 0.75),
+    q90_resid_trend = quantile(resid_trend, probs = 0.9),
+    q99_resid_trend = quantile(resid_trend, probs = 0.99)
+  ) %>%
+  as.list()
 
 # export ------------------------------------------------------------------
 
 list(
-  calibration = list(
-    values = df_pred,
-    site = df_pred_site,
-    deploy = df_pred_deploy
+  calib = list(
+    values = df_calib,
+    deploy = df_calib_deploy,
+    catchment = df_calib_catchment,
+    huc8 = df_calib_huc8,
+    summary = df_calib_summary
   ),
-  validation = list(
+  valid = list(
     values = df_valid,
-    site = df_valid_site,
-    deploy = df_valid_deploy
+    deploy = df_calib_deploy,
+    catchment = df_valid_catchment,
+    huc8 = df_valid_huc8,
+    summary = df_valid_summary
   )
 ) %>%
   saveRDS(file.path(config$wd, "model-diagnostics.rds"))
@@ -207,27 +311,27 @@ list(
 
 
 # RMSE
-sqrt(mean(df_pred$resid^2))
-sqrt(mean(df_pred$resid_Y^2))
-df_pred_site %>%
+sqrt(mean(df_calib$resid^2))
+sqrt(mean(df_calib$resid_trend^2))
+df_calib_site %>%
   arrange(desc(rmse))
-df_pred_site %>%
-  arrange(desc(rmse_Y))
-df_pred_huc %>%
+df_calib_site %>%
+  arrange(desc(rmse_trend))
+df_calib_huc %>%
   arrange(desc(rmse))
-df_pred_huc %>%
-  arrange(desc(rmse_Y))
+df_calib_huc %>%
+  arrange(desc(rmse_trend))
 
 
-df_pred %>%
+df_calib %>%
   ggplot(aes(resid)) +
   geom_histogram()
 
-df_pred %>%
+df_calib %>%
   ggplot(aes(resid)) +
   stat_ecdf()
 
-df_pred %>%
+df_calib %>%
   ggplot(aes(temp, pred)) +
   geom_abline() +
   geom_point(size = 1, alpha = 0.2) +
@@ -237,7 +341,7 @@ df_pred %>%
   ) +
   theme(aspect.ratio = 1)
 
-df_pred %>%
+df_calib %>%
   ggplot(aes(temp, Y)) +
   geom_abline() +
   geom_point(size = 1, alpha = 0.2) +
@@ -247,7 +351,7 @@ df_pred %>%
   ) +
   theme(aspect.ratio = 1)
 
-df_pred_site %>%
+df_calib_site %>%
   arrange(desc(rmse)) %>%
   head(10) %>%
   unnest(data) %>%
@@ -256,7 +360,7 @@ df_pred_site %>%
   geom_point(aes(y = temp), size = 1, color = "orangered") +
   facet_wrap(~ site, scales = "free")
 
-df_pred_site %>%
+df_calib_site %>%
   arrange(rmse) %>%
   head(10) %>%
   unnest(data) %>%
@@ -266,8 +370,8 @@ df_pred_site %>%
   facet_wrap(~ site, scales = "free")
 
 # w/o autoregressive term
-df_pred_site %>%
-  arrange(desc(rmse_Y)) %>%
+df_calib_site %>%
+  arrange(desc(rmse_trend)) %>%
   head(12) %>%
   unnest(data) %>%
   ggplot(aes(date)) +
@@ -276,9 +380,9 @@ df_pred_site %>%
   facet_wrap(~ site, scales = "free") +
   labs(x = "Date", y = "Predicted Temperature (degC) w/o Autoregressive Term")
 
-df_pred_site %>%
+df_calib_site %>%
   filter(n > 100) %>%
-  arrange(rmse_Y) %>%
+  arrange(rmse_trend) %>%
   head(12) %>%
   unnest(data) %>%
   ggplot(aes(date)) +
@@ -286,10 +390,10 @@ df_pred_site %>%
   geom_point(aes(y = temp), size = 1, color = "orangered", shape = 1) +
   facet_wrap(~ site, scales = "free")
 
-table(df_pred$pred > df_pred$temp) / nrow(df_pred)
-table(df_pred$Y > df_pred$temp) / nrow(df_pred)
+table(df_calib$pred > df_calib$temp) / nrow(df_calib)
+table(df_calib$Y > df_calib$temp) / nrow(df_calib)
 
-df_pred %>%
+df_calib %>%
   ungroup() %>%
   select(temp, Y, pred) %>%
   gather(var, value) %>%
@@ -305,7 +409,7 @@ df_pred %>%
     )
   )
 
-df_pred_huc %>%
+df_calib_huc %>%
   arrange(desc(rmse)) %>%
   head(1) %>%
   unnest(data) %>%
@@ -314,8 +418,8 @@ df_pred_huc %>%
   geom_point(aes(y = temp), color = "orangered", size = 1) +
   facet_wrap(~site, scales = "free")
 
-df_pred_huc %>%
-  arrange(desc(rmse_Y)) %>%
+df_calib_huc %>%
+  arrange(desc(rmse_trend)) %>%
   head(2) %>%
   unnest(data) %>%
   ggplot(aes(date)) +
@@ -327,32 +431,32 @@ df_pred_huc %>%
 
 # RMSE
 sqrt(mean(df_valid$resid^2))
-sqrt(mean(df_valid$resid_Y^2))
+sqrt(mean(df_valid$resid_trend^2))
 df_valid_site %>%
   arrange(desc(rmse))
 df_valid_site %>%
-  arrange(desc(rmse_Y))
+  arrange(desc(rmse_trend))
 
 df_valid %>%
-  select(resid, resid_Y) %>%
+  select(resid, resid_trend) %>%
   gather(var, value) %>%
   ggplot(aes(value, fill = var)) +
   geom_histogram() +
   facet_wrap(~ var, labeller = labeller(
     var = c(
       "resid" = "w/ autoregressive",
-      "resid_Y" = "w/o autoregressive"
+      "resid_trend" = "w/o autoregressive"
     )
   ))
 
 df_valid %>%
-  select(resid, resid_Y) %>%
+  select(resid, resid_trend) %>%
   gather(var, value) %>%
   ggplot(aes(value, color = var)) +
   stat_ecdf() +
   scale_color_discrete("", labels = c(
     "resid" = "w/ autoregressive",
-    "resid_Y" = "w/o autoregressive"
+    "resid_trend" = "w/o autoregressive"
   ))
 
 df_valid %>%
@@ -396,7 +500,7 @@ df_valid_site %>%
 
 # w/o autoregressive term
 df_valid_site %>%
-  arrange(desc(rmse_Y)) %>%
+  arrange(desc(rmse_trend)) %>%
   head(10) %>%
   unnest(data) %>%
   ggplot(aes(date)) +
@@ -405,7 +509,7 @@ df_valid_site %>%
   facet_wrap(~ site, scales = "free")
 
 df_valid_site %>%
-  arrange(rmse_Y) %>%
+  arrange(rmse_trend) %>%
   head(10) %>%
   unnest(data) %>%
   ggplot(aes(date)) +
@@ -526,7 +630,7 @@ out$results$summary %>%
   inner_join(B.site_labels, by = c("param" = "Parameter")) %>%
   filter(cov == "intercept.site") %>%
   select(site, intercept_mean = mean) %>%
-  left_join(df_pred_site, by = "site") %>%
+  left_join(df_calib_site, by = "site") %>%
   arrange(desc(intercept_mean)) %>%
   head(10) %>%
   unnest(data) %>%
