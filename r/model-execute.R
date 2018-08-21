7af2e9f42593a4c495ffc06d5f23dcb3d87ceb00
Jeff Walker <walker.jeff.d@gmail.com>
2018-08-21 10:12:02 -0400

    update model-execute and jags model

--- r/model-execute.R
+++ r/model-execute.R
@@ -3,7 +3,7 @@
 # -> {wd}/model-output.rds
 
 start <- lubridate::now(tzone = "US/Eastern")
-cat("starting model-execute:", as.character(start, tz = "US/Eastern"), "\n")
+cat("starting model-execute: ", as.character(start, tz = "US/Eastern"), "\n", sep = "")
 
 suppressPackageStartupMessages(library(RPostgreSQL))
 suppressPackageStartupMessages(library(tidyverse))
@@ -19,39 +19,24 @@ config <- load_config()
 
 # load data ---------------------------------------------------------------
 
+cat("loading model-input.rds...")
 inp <- readRDS(file.path(config$wd, "model-input.rds"))
 
-# HUC2 <- "02"
-# df <- inp$train %>%
-#   mutate(
-#     intercept = 1,
-#     intercept.site = 1,
-#     intercept.year = 1
-#   ) %>%
-#   filter(str_sub(huc8, 1, 2) == HUC2) %>%
-#   mutate(
-#     site_i = as.numeric(as.factor(site)),
-#     huc8_i = as.numeric(as.factor(huc8)),
-#     year_i = as.numeric(as.factor(year))
-#   ) %>%
-#   arrange(huc8_i, site_i, date) %>%
-#   mutate(
-#     delta_date = as.numeric(difftime(date, lag(date), units = "day")),
-#     new_series = delta_date != 1,
-#     new_site = coalesce(site_i != lag(site_i), TRUE),
-#     new_year = coalesce(year != lag(year), TRUE),
-#     new_deploy = 1 * (new_series | new_site | new_year),
-#     deploy_id = cumsum(new_deploy)
-#   )
+df_inp <- inp$train
 
-df <- inp$train %>%
+df <- df_inp %>%
   mutate(
     intercept = 1,
     intercept.site = 1,
+    intercept.huc = 1,
     intercept.year = 1
   )
 first_rows <- which(df$new_deploy == 1)
 eval_rows <- which(df$new_deploy == 0)
+
+cat("done\n")
+
+cat("setting up model params...")
 
 cov.list <- list(
   fixed.ef = c(
@@ -81,7 +66,7 @@ cov.list <- list(
     "temp7p"
   ),
   huc.ef = c(
-    "intercept.site",
+    "intercept.huc",
     "airTemp",
     "temp7p"
   ),
@@ -113,97 +98,85 @@ params <- c(
 # FIXED EFFECTS
 X.0 <- df %>%
   select(one_of(cov.list$fixed.ef))
-variables.fixed <- names(X.0)
-K.0 <- length(variables.fixed)
+var.0 <- names(X.0)
 
 # RANDOM SITE EFFECTS
 X.site <- df %>%
   select(one_of(cov.list$site.ef))
-variables.site <- names(X.site)
-sites <- df$site_id
-J <- length(unique(sites))
-K <- length(variables.site)
-n <- nrow(df)
-W.site <- diag(K)
+var.site <- names(X.site)
+id.site <- df$featureid_id
 
 # RANDOM HUC EFFECTS
-hucs <- df$huc8_id
-M <- length(unique(hucs))
-W.huc <- diag(K)
+X.huc <- df %>%
+  select(one_of(cov.list$huc.ef))
+var.huc <- names(X.huc)
+id.huc <- df$huc8_id
 
 # RANDOM YEAR EFFECTS
 X.year <- df %>%
   select(one_of(cov.list$year.ef))
-variables.year <- names(X.year)
-years <- df$year_id
-Ti <- length(unique(years))
-L <- length(variables.year)
-W.year <- diag(L)
+var.year <- names(X.year)
+id.year <- df$year_id
+
 data.list <- list(
-  n = n,     # no. of observations
-  J = J,     # no. of sites
-  K = K,     # no. of site effect variables
-  Ti = Ti,   # no. of years
-  L = L,     # no. of year effect variables
-  M = M,     # no. of hucs
-  K.0 = K.0, # no. of fixed effect variables
-  X.0 = as.matrix(X.0),  # fixed effects data
-  W.site = W.site,       # cov matrix for site effects
-  W.year = W.year,       # cov matrix for year effects
-  W.huc = W.huc,         # cov matrix for huc effects
-  temp = df$temp,        # temp observations
-  evalRows = eval_rows,               # evaluation row indices
-  nEvalRows = length(eval_rows),      # no. of evaluation rows
-  firstObsRows = first_rows,          # first row indices
-  nFirstObsRows = length(first_rows), # no. of first row indices
-  X.site = as.matrix(X.site),  # site effects data
-  X.year = as.matrix(X.year),  # year effects data
-  site = sites,  # site group indices
-  huc = hucs,    # huc group indices
-  year = years   # year group indices
+  temp = df$temp,                    # temp observations
+  n = nrow(df),                      # no. of observations
+
+  X.0 = as.matrix(X.0),              # fixed effects data
+  K.0 = length(var.0),               # no. of fixed effect variables
+
+  site = id.site,                    # site group indices
+  X.site = as.matrix(X.site),        # site effects data
+  K.site = length(var.site),         # no. of site effect variables
+  N.site = max(id.site),             # no. of sites
+  W.site = diag(length(var.site)),   # cov matrix for site effects
+
+  huc = id.huc,                      # huc group indices
+  X.huc = as.matrix(X.huc),          # huc effects data
+  K.huc = length(var.huc),           # no. of huc effect variables
+  N.huc = max(id.huc),               # no. of hucs
+  W.huc = diag(length(var.huc)),     # cov matrix for huc effects
+
+  year = id.year,                    # year group indices
+  X.year = as.matrix(X.year),        # year effects data
+  K.year = length(var.year),         # no. of year effect variables
+  N.year = max(id.year),             # no. of years
+  W.year = diag(length(var.year)),   # cov matrix for year effects
+
+  evalRows = eval_rows,              # evaluation row indices
+  nEvalRows = length(eval_rows),     # no. of evaluation rows
+  firstObsRows = first_rows,         # first row indices
+  nFirstObsRows = length(first_rows) # no. of first row indices
 )
 
-cat(n, J, K, Ti, L, M, K.0, "\n")
+# cat(n, J, K, Ti, L, M, K.0, "\n")
 
 # group ids
 ids <- list(
   site = df %>%
-    select(site, site_id) %>%
+    select(featureid, featureid_id) %>%
     distinct() %>%
-    arrange(site_id),
+    arrange(featureid_id),
+  huc = df %>%
+    select(huc8, huc8_id) %>%
+    distinct() %>%
+    arrange(huc8_id),
   year = df %>%
     select(year, year_id) %>%
     distinct() %>%
-    arrange(year_id),
-  huc8 = df %>%
-    select(huc8, huc8_id) %>%
-    distinct() %>%
-    arrange(huc8_id)
+    arrange(year_id)
 )
 
-
-#
-# tau.huc <- as.matrix(
-#   cbind(
-#     c(runif(1,3.5,4.5), runif(1,-2.5, -2), runif(1,-3,-2.5)),
-#     c(0, runif(1,10, 20), runif(1,-0.5,-0.1)),
-#     c(0, 0, runif(1,3,10))
-#   )
-# )
-#
-# inits.list <- list(
-#   list(sigma = runif(1)),
-#   list(sigma = runif(1)),
-#   list(sigma = runif(1))
-# )
-
+cat("done\n")
 
 # copy jags file ----------------------------------------------------------
 
+cat("copying temp-model.jags...")
 copied <- file.copy("jags/temp-model.jags", file.path(config$wd, "temp-model.jags"), overwrite = TRUE)
 if (!copied) {
   stop("Failed to copy JAGS model definition file")
 }
+cat("done\n")
 
 # run model ---------------------------------------------------------------
 
@@ -225,20 +198,24 @@ cat("done\n")
 
 # https://www.rdocumentation.org/packages/jagsUI/versions/1.4.4/topics/jags.basic
 
-out <- list(
+
+# export ------------------------------------------------------------------
+
+cat("saving to model-output.rds...")
+list(
   df = df,
   ids = ids,
   data = data.list,
   params = params,
   covs = cov.list,
   results = jm
-)
-
-cat("saving results to", file.path(config$wd, "model-output.rds"), "...")
-out %>%
+) %>%
   saveRDS(file.path(config$wd, "model-output.rds"))
 cat("done\n")
 
+
+# end ---------------------------------------------------------------------
+
 end <- lubridate::now(tzone = "US/Eastern")
 elapsed <- as.numeric(difftime(end, start, tz = "US/Eastern", units = "sec"))
-cat("finished model-execute:", as.character(end, tz = "US/Eastern"), "( elapsed =", round(elapsed / 60, digits = 1), "min )\n")
+cat("finished model-execute: ", as.character(end, tz = "US/Eastern"), " (elapsed = ", round(elapsed / 60, digits = 1), " min)\n", sep = "")
