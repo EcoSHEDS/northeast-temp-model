7eaafca3cbe963c96681c2fbfd0651139be75e91
Jeff Walker <walker.jeff.d@gmail.com>
2018-08-21 10:11:01 -0400

    update model-run

--- run-model.sh
+++ run-model.sh
@@ -4,41 +4,48 @@
 
 set -eu
 
+cd scripts
 # create list of locations to exclude (tidal + impounded)
-./scripts/locations-exclude.sh            # -> locations-tidal.txt, locations-impoundment.txt, locations-exclude.txt
+./locations-exclude.sh            # -> locations-tidal.txt, locations-impoundment.txt, locations-exclude.txt
 
 # calculate distances from each location to the nearest flowline and catchment pour point
-./scripts/locations-flowlines-distance.sh # -> locations-flowlines-distance.csv
+./locations-flowlines-distance.sh # -> locations-flowlines-distance.csv
 
+
+cd ../r
 # retrieve catchment-huc8 list from database
-Rscript r/data-huc.R                      # -> data-huc.rds
+Rscript data-huc.R                # -> data-huc.rds
 
 # retrieve all stream temperature observations from database
-Rscript r/data-db.R                       # -> data-db.rds, daymet-featureid_year.csv
+Rscript data-db.R                 # -> data-db.rds, daymet-featureid_year.csv
 
+
+cd ../scripts
 # retrieve daymet data from database
-./scripts/data-daymet.sh                  # -> data-daymet.csv
+./data-daymet.sh                  # -> data-daymet.csv
 
+
+cd ../r
 # retrieve covariate data from database
-Rscript r/data-covariates.R               # -> covariates.rds
+Rscript data-covariates.R               # -> data-covariates.rds
 
 # merge, qaqc, and process input datasets
-Rscript r/data-clean.R                    # -> data-clean.rds
+Rscript data-clean.R                    # -> data-clean.rds
 
 # determine spring/fall breakpoints
-script r/data-breakpoints.R               # -> data-breakpoints.rds
+Rscript data-breakpoints.R              # -> data-breakpoints.rds
 
 # prepare model input dataset
-Rscript r/model-input.R                   # -> model-input.rds
+Rscript model-input.R                   # -> model-input.rds
 
 # fit the model using JAGS
-Rscript r/model-execute.R                 # -> model-output.rds
+Rscript model-execute.R                 # -> model-output.rds
 
 # generate diagnostics dataset and plots
-Rscript r/model-diagnostics.R             # -> model-diagnostics.rds
+Rscript model-diagnostics.R             # -> model-diagnostics.rds
 
 # generate annual derived metrics of predictions for each catchment
-Rscript r/model-predict-year.R            # -> model-predict-year.rds
+Rscript model-predict-year.R            # -> model-predict-year.rds
 
 # calculate overall mean derived metrics of predictions for each catchment
-Rscript r/model-predict-derived.R         # -> model-predict-derived.[rds,csv]
+Rscript model-predict-derived.R         # -> model-predict-derived.[rds,csv]
