4ac454fb20e5af92c661a3f59de9d92bc1c1337b
Jeff Walker <walker.jeff.d@gmail.com>
2018-11-28 11:07:56 -0500

    refactor model-diagnostics, make ar1-fit secondary

--- r/model-diagnostics.R
+++ r/model-diagnostics.R
@@ -31,7 +31,8 @@ out <- readRDS(file.path(config$wd, "model-output.rds"))
 
 cat("done\n")
 
-# calibration -------------------------------------------------------------
+
+# calib dataset -----------------------------------------------------------
 
 cat("calculating calibration statistics...")
 
@@ -54,108 +55,27 @@ Y.huc8 <- rowSums(X.huc8 * B.huc8)
 Y <- Y.fixed + Y.catchment + Y.huc8 + Y.year
 
 df_calib <- data_frame(
+  dataset = "calib",
   date = out$df$date,
   huc8 = out$df$huc8,
   featureid = out$df$featureid,
   year = out$df$year,
   deploy_id = out$df$deploy_id,
   obs = out$df$temp,
-  trend = Y
+  pred = Y
 ) %>%
-  group_by(deploy_id) %>%
   mutate(
-    deploy_row = row_number(),
-    resid_trend = obs - trend,
-    resid_lag = coalesce(lag(resid_trend), 0),
-    pred = if_else(deploy_row == 1, trend, trend + out$results$mean$B.ar1 * resid_lag),
     resid = obs - pred
-  ) %>%
-  ungroup()
-
-df_calib_deploy <- df_calib %>%
-  group_by(featureid, year, deploy_id) %>%
-  nest() %>%
-  mutate(
-    n = map_int(data, nrow),
-    rmse = map_dbl(data, function (x) {
-      sqrt(mean(x$resid^2))
-    }),
-    rmse_trend = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_trend^2))
-    })
   )
-
-df_calib_catchment <- df_calib %>%
-  group_by(featureid) %>%
-  nest() %>%
-  mutate(
-    n = map_int(data, nrow),
-    rmse = map_dbl(data, function (x) {
-      sqrt(mean(x$resid^2))
-    }),
-    rmse_trend = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_trend^2))
-    })
-  )
-
-df_calib_huc8 <- df_calib %>%
-  group_by(huc8) %>%
-  nest() %>%
-  mutate(
-    n = map_int(data, nrow),
-    rmse = map_dbl(data, function (x) {
-      sqrt(mean(x$resid^2))
-    }),
-    rmse_trend = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_trend^2))
-    })
-  )
-
-df_calib_summary <- df_calib %>%
-  summarise(
-    n_obs = n(),
-    n_catchment = length(unique(featureid)),
-    n_huc8 = length(unique(huc8)),
-    n_deploy = length(unique(deploy_id)),
-    n_year = length(unique(year)),
-
-    rmse = sqrt(mean(resid ^ 2)),
-    mean_resid = mean(resid),
-    median_resid = median(resid),
-    mean_abs_resid = mean(abs(resid)),
-    median_abs_resid = median(abs(resid)),
-    min_resid = min(resid),
-    max_resid = max(resid),
-    q01_resid = quantile(resid, probs = 0.01),
-    q10_resid = quantile(resid, probs = 0.1),
-    q25_resid = quantile(resid, probs = 0.25),
-    q75_resid = quantile(resid, probs = 0.75),
-    q90_resid = quantile(resid, probs = 0.9),
-    q99_resid = quantile(resid, probs = 0.99),
-
-    rmse_trend = sqrt(mean(resid_trend ^ 2)),
-    mean_resid_trend = mean(resid_trend),
-    median_resid_trend = median(resid_trend),
-    mean_abs_resid_trend = mean(abs(resid_trend)),
-    median_abs_resid_trend = median(abs(resid_trend)),
-    min_resid_trend = min(resid_trend),
-    max_resid_trend = max(resid_trend),
-    q01_resid_trend = quantile(resid_trend, probs = 0.01),
-    q10_resid_trend = quantile(resid_trend, probs = 0.1),
-    q25_resid_trend = quantile(resid_trend, probs = 0.25),
-    q75_resid_trend = quantile(resid_trend, probs = 0.75),
-    q90_resid_trend = quantile(resid_trend, probs = 0.9),
-    q99_resid_trend = quantile(resid_trend, probs = 0.99)
-  ) %>%
-  as.list()
 
 cat("done\n")
 
-# validation --------------------------------------------------------------
+
+# valid dataset -----------------------------------------------------------
 
 cat("calculating validation statistics...")
 
-df_valid <- readRDS(file.path(config$wd, "model-input.rds"))$test %>%
+df_input_valid <- readRDS(file.path(config$wd, "model-input.rds"))$test %>%
   select(-featureid_id, -huc8_id, -year_id) %>%
   left_join(out$ids$site, by = "featureid") %>%
   left_join(out$ids$year, by = "year") %>%
@@ -165,17 +85,17 @@ df_valid <- readRDS(file.path(config$wd, "model-input.rds"))$test %>%
   )
 # summary(df_valid)
 
-X.fixed_valid <- df_valid %>%
+X.fixed_valid <- df_input_valid %>%
   mutate(intercept = 1) %>%
   select(one_of(out$covs$fixed.ef))
 B.fixed_valid <- out$results$mean$B.0
 Y.fixed_valid <- (as.matrix(X.fixed_valid) %*% as.matrix(B.fixed_valid))[, 1]
 
-X.catchment_valid <- df_valid %>%
+X.catchment_valid <- df_input_valid %>%
   mutate(intercept.site = 1) %>%
   select(one_of(out$covs$site.ef))
 B.catchment.mean <- colMeans(out$results$mean$B.site)
-B.catchment_valid <- out$results$mean$B.site[df_valid$featureid_id, ]
+B.catchment_valid <- out$results$mean$B.site[df_input_valid$featureid_id, ]
 for (i in seq_along(B.catchment.mean)) {
   B.catchment_valid[is.na(B.catchment_valid[, i]), i] <- B.catchment.mean[i]
 }
@@ -183,17 +103,17 @@ Y.catchment_valid <- rowSums(X.catchment_valid * B.catchment_valid)
 
 X.huc8_valid <- X.catchment_valid
 B.huc8.mean <- colMeans(out$results$mean$B.huc)
-B.huc8_valid <- out$results$mean$B.huc[df_valid$huc8_id, ]
+B.huc8_valid <- out$results$mean$B.huc[df_input_valid$huc8_id, ]
 for (i in seq_along(B.huc8.mean)) {
   B.huc8_valid[is.na(B.huc8_valid[, i]), i] <- B.huc8.mean[i]
 }
 Y.huc8_valid <- rowSums(X.huc8_valid * B.huc8_valid)
 
-X.year_valid <- df_valid %>%
+X.year_valid <- df_input_valid %>%
   mutate(intercept.year = 1) %>%
   select(one_of(out$covs$year.ef))
 B.year.mean <- colMeans(out$results$mean$B.year)
-B.year_valid <- as.matrix(out$results$mean$B.year[df_valid$year_id, ])
+B.year_valid <- as.matrix(out$results$mean$B.year[df_input_valid$year_id, ])
 for (i in seq_along(B.year.mean)) {
   B.year_valid[is.na(B.year_valid[, i]), i] <- B.year.mean[i]
 }
@@ -201,97 +121,108 @@ Y.year_valid <- rowSums(X.year_valid * B.year_valid)
 
 Y_valid <- Y.fixed_valid + Y.huc8_valid + Y.catchment_valid + Y.year_valid
 
-df_valid <- df_valid %>%
+df_valid <- df_input_valid %>%
+  mutate(dataset = "valid") %>%
+  select(dataset, date, huc8, featureid, year, deploy_id, obs) %>%
   mutate(
-    trend = Y_valid
-  ) %>%
-  group_by(deploy_id) %>%
-  mutate(
-    deploy_row = row_number(),
-    resid_trend = obs - trend,
-    resid_lag = coalesce(lag(resid_trend), 0),
-    pred = if_else(deploy_row == 1, trend, trend + out$results$mean$B.ar1 * resid_lag),
+    pred = Y_valid,
     resid = obs - pred
+  )
+cat("done\n")
+
+# merge and calculate group stats -----------------------------------------
+
+cat("merging calibration and validation datasets...")
+df <- bind_rows(df_calib, df_valid) %>%
+  group_by(dataset, deploy_id) %>%
+  mutate(
+    resid_lag = coalesce(lag(resid), 0),
+    pred_ar1 = pred + out$results$mean$B.ar1 * resid_lag,
+    resid_ar1 = obs - pred_ar1
   ) %>%
-  ungroup() %>%
-  select(date, huc8, featureid, year, deploy_id, obs, trend, deploy_row, resid_trend, resid_lag, pred, resid)
+  ungroup()
+cat("done\n")
 
-df_valid_deploy <- df_valid %>%
-  group_by(featureid, year, deploy_id) %>%
+cat("computing stats by deployment...")
+df_deploy <- df %>%
+  group_by(dataset, featureid, year, deploy_id) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_trend = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_trend^2))
+    rmse_ar1 = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_ar1^2))
     })
   )
+cat("done\n")
 
-df_valid_catchment <- df_valid %>%
-  group_by(featureid) %>%
+cat("computing stats by catchment...")
+df_catchment <- df %>%
+  group_by(dataset, featureid) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_trend = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_trend^2))
+    rmse_ar1 = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_ar1^2))
     })
   )
+cat("done\n")
 
-df_valid_huc8 <- df_valid %>%
-  group_by(huc8) %>%
+cat("computing stats by huc8...")
+df_huc8 <- df %>%
+  group_by(dataset, huc8) %>%
   nest() %>%
   mutate(
     n = map_int(data, nrow),
     rmse = map_dbl(data, function (x) {
       sqrt(mean(x$resid^2))
     }),
-    rmse_trend = map_dbl(data, function (x) {
-      sqrt(mean(x$resid_trend^2))
+    rmse_ar1 = map_dbl(data, function (x) {
+      sqrt(mean(x$resid_ar1^2))
     })
   )
+cat("done\n")
 
-df_valid_summary <- df_valid %>%
-  summarise(
-    n_obs = n(),
-    n_catchment = length(unique(featureid)),
-    n_huc8 = length(unique(huc8)),
-    n_deploy = length(unique(deploy_id)),
-    n_year = length(unique(year)),
+resid_stats <- function (x) {
+  list(
+    rmse = sqrt(mean(x ^ 2)),
+    mean_resid = mean(x),
+    median_resid = median(x),
+    mean_abs_resid = mean(abs(x)),
+    median_abs_resid = median(abs(x)),
+    min_resid = min(x),
+    max_resid = max(x),
+    q01_resid = quantile(x, probs = 0.01),
+    q10_resid = quantile(x, probs = 0.1),
+    q25_resid = quantile(x, probs = 0.25),
+    q75_resid = quantile(x, probs = 0.75),
+    q90_resid = quantile(x, probs = 0.9),
+    q99_resid = quantile(x, probs = 0.99)
+  )
+}
 
-    rmse = sqrt(mean(resid ^ 2)),
-    mean_resid = mean(resid),
-    median_resid = median(resid),
-    mean_abs_resid = mean(abs(resid)),
-    median_abs_resid = median(abs(resid)),
-    min_resid = min(resid),
-    max_resid = max(resid),
-    q01_resid = quantile(resid, probs = 0.01),
-    q10_resid = quantile(resid, probs = 0.1),
-    q25_resid = quantile(resid, probs = 0.25),
-    q75_resid = quantile(resid, probs = 0.75),
-    q90_resid = quantile(resid, probs = 0.9),
-    q99_resid = quantile(resid, probs = 0.99),
+cat("calculating stats...")
+stats <- lapply(c("calib", "valid"), function (dataset_) {
+  x <- df %>% filter(dataset == dataset_)
 
-    rmse_trend = sqrt(mean(resid_trend ^ 2)),
-    mean_resid_trend = mean(resid_trend),
-    median_resid_trend = median(resid_trend),
-    mean_abs_resid_trend = mean(abs(resid_trend)),
-    median_abs_resid_trend = median(abs(resid_trend)),
-    min_resid_trend = min(resid_trend),
-    max_resid_trend = max(resid_trend),
-    q01_resid_trend = quantile(resid_trend, probs = 0.01),
-    q10_resid_trend = quantile(resid_trend, probs = 0.1),
-    q25_resid_trend = quantile(resid_trend, probs = 0.25),
-    q75_resid_trend = quantile(resid_trend, probs = 0.75),
-    q90_resid_trend = quantile(resid_trend, probs = 0.9),
-    q99_resid_trend = quantile(resid_trend, probs = 0.99)
-  ) %>%
-  as.list()
+  x_n <- list(
+    n_obs = nrow(x),
+    n_catchment = length(unique(x$featureid)),
+    n_huc8 = length(unique(x$huc8)),
+    n_deploy = length(unique(x$deploy_id)),
+    n_year = length(unique(x$year))
+  )
+  x_resid <- resid_stats(x$resid)
+  x_resid_ar1 <- resid_stats(x$resid_ar1)
+  names(x_resid_ar1) <- paste0(names(x_resid_ar1), "_ar1")
+  c(x_n, x_resid, x_resid_ar1)
+}) %>%
+  setNames(nm = c("calib", "valid"))
 cat("done\n")
 
 # export ------------------------------------------------------------------
@@ -299,18 +230,18 @@ cat("done\n")
 cat("saving to model-diagnostics.rds...")
 list(
   calib = list(
-    values = df_calib,
-    deploy = df_calib_deploy,
-    catchment = df_calib_catchment,
-    huc8 = df_calib_huc8,
-    summary = df_calib_summary
+    values = filter(df, dataset == "calib"),
+    deploy = filter(df_deploy, dataset == "calib"),
+    catchment = filter(df_catchment, dataset == "calib"),
+    huc8 = filter(df_huc8, dataset == "calib"),
+    summary = stats$calib
   ),
   valid = list(
-    values = df_valid,
-    deploy = df_valid_deploy,
-    catchment = df_valid_catchment,
-    huc8 = df_valid_huc8,
-    summary = df_valid_summary
+    values = filter(df, dataset == "valid"),
+    deploy = filter(df_deploy, dataset == "valid"),
+    catchment = filter(df_catchment, dataset == "valid"),
+    huc8 = filter(df_huc8, dataset == "valid"),
+    summary = stats$valid
   )
 ) %>%
   saveRDS(file.path(config$wd, "model-diagnostics.rds"))
